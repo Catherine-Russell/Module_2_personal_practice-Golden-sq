0e3b433543433f22ff2d463b27b0e663c772ad58
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-09-21 16:32:02 +0100

    After Pairing session with Gina

--- Testing_Classes/lib/to_do_organiser.py
+++ Testing_Classes/lib/to_do_organiser.py
@@ -6,7 +6,23 @@ class To_do_tracker():
         #add a new tasl to self.task_list
         #input string type str
         #output None
-        self.task_lst .append(string)
+        if string == "":
+            raise Exception("Task can not be empty string")
+        elif type(string) != str:
+            raise Exception("Task must be a string")
+        else:
+            self.task_lst.append(string)
+
+    def add_multiple_tasks(self, tasks):
+        new_tasks = tasks.split(", ")
+        for task in new_tasks:
+            if "and" not in task:
+                self.add_task(task)
+            else:
+                
+                last_tasks = task.split(" and ")
+                for task in last_tasks:
+                    self.add_task(task)
     
     def lst_tasks(self):
         #return list of tasks
@@ -20,3 +36,5 @@ class To_do_tracker():
         #output none
         if task in self.task_lst:
             self.task_lst.remove(task)
+        else:
+            return f"This task is not in your To Do List. Please choose from: {self.task_lst}"
--- Testing_Classes/tests/test_to_do_organiser.py
+++ Testing_Classes/tests/test_to_do_organiser.py
@@ -7,7 +7,11 @@ add two task and check the task list ==> [task1,task2]
 
 add two task and remove the second one and call check list ==> [task1]
 add two task and remove the fist one and call check list ==> [task2]
-remove a task never added ==> return string "choose a task from thisn list' and then return the list
+remove a task never added ==> return string "choose a task from this list' and then return the list
+clear the list - remove more than 1 task ==> []
+
+
+
 
 Edge cases
 add empty string ==> raise Exception "task can not be empty string"
@@ -48,4 +52,40 @@ def test_add_two_task_remove_first_call_check_list():
     tracker.remove_completed_task("cook")
     assert tracker.lst_tasks() == ["do laundry"]
 
+def test_remove_more_than_one_task():
+    tracker = To_do_tracker()
+    tracker.add_task("do laundry")
+    tracker.add_task("cook")
+    tracker.remove_completed_task("cook")
+    tracker.remove_completed_task("do laundry")
+    assert tracker.lst_tasks() == []
 
+def test_remove_task_not_in_list():
+    tracker = To_do_tracker()
+    tracker.add_task("do laundry")
+    tracker.add_task("cook")
+    assert tracker.remove_completed_task("wash dishes") == "This task is not in your To Do List. Please choose from: ['do laundry', 'cook']"
+
+def test_add_empty_string_raises_error():
+    tracker = To_do_tracker()
+    with pytest.raises(Exception) as e:
+        tracker.add_task("")
+    error_message = str(e.value)
+    assert error_message == "Task can not be empty string"
+
+def test_add_non_string_type_raises_error():
+    tracker = To_do_tracker()
+    with pytest.raises(Exception) as e:
+        tracker.add_task(1234)
+    error_message = str(e.value)
+    assert error_message == "Task must be a string"
+
+def test_add_multiple_tasks_sep_with_comma():
+    tracker = To_do_tracker()
+    tracker.add_multiple_tasks("cooking, sleeping, reading")
+    assert tracker.lst_tasks() == ["cooking", "sleeping", "reading"] 
+
+def test_add_multiple_tasks_sep_with_and():
+    tracker = To_do_tracker()
+    tracker.add_multiple_tasks("cooking, sleeping and reading")
+    assert tracker.lst_tasks() == ["cooking", "sleeping", "reading"] 
